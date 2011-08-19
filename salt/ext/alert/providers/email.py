c3db0b8ad62ea41ba238331ed67afa3bd016dd2b
Erik Nolte <enolte@beyondoblivion.com>
2011-08-19 00:17:43 -0600

    Implement email alert notification

--- salt/ext/alert/providers/email.py
+++ salt/ext/alert/providers/email.py
@@ -1,3 +1,5 @@
+import smtplib
+
 import salt.log
 
 log = salt.log.getLogger(__name__)
@@ -8,14 +10,22 @@ class EmailProvider(object):
         self.protocol = 'email'
         self.smtp_server = opts.get('host')
         self.smtp_port = opts.get('port', 25)
-        self.user = opts.get('user')
+        self.user = opts.get('user', 'salt-alert')
         self.password = opts.get('password')
 
         if not self.smtp_server:
             raise ValueError('email provider missing "host" option')
 
     def send(self, addr, summary, body):
-        log.error('XXXX send email alert')
-        log.error('XXXX to %s', addr)
-        log.error('XXXX subject %s', summary)
-        log.error('XXXX body %s', body)
+        msg = 'From: {}\nTo: {}\nSubject: {}\n\n{}'.format(
+                    self.user, addr, summary, body)
+        msg = msg.replace('\n', '\r\n')
+        try:
+            log.trace('connect to %s %s', self.smtp_server, self.smtp_port)
+            smtp = smtplib.SMTP(self.smtp_server, self.smtp_port)
+            log.trace('send from %s to %s: %s', self.user, addr, msg)
+            smtp.sendmail(self.user, addr, msg)
+            smtp.quit()
+            log.trace('email alert sent to %s', addr)
+        except Exception, ex:
+            log.error('failed to send email alert', exc_info=ex)
